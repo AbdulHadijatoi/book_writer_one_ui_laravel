a56250f3569abd2a17696cad71e4dff5383d6a08
H∆DI <hadi@thiqatech.com>
2022-02-24 13:36:26 +0500

    removed extra stuff from dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,108 +41,13 @@
         </div>
       @endif
      
-      <div class="row row-deck">
-        <div class="col-sm-6 col-xxl-3">
-          <div class="block block-rounded d-flex flex-column">
-            <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-              <dl class="mb-0">
-                <dt class="fs-3 fw-bold">32</dt>
-                <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Pending Orders</dd>
-              </dl>
-              <div class="item item-rounded-lg bg-body-light">
-                <i class="far fa-gem fs-3 text-primary"></i>
-              </div>
-            </div>
-            <div class="bg-body-light rounded-bottom">
-              <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                <span>View all orders</span>
-                <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-              </a>
-            </div>
-          </div>
-          <!-- END Pending Orders -->
-        </div>
-        <div class="col-sm-6 col-xxl-3">
-          <!-- New Customers -->
-          <div class="block block-rounded d-flex flex-column">
-            <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-              <dl class="mb-0">
-                <dt class="fs-3 fw-bold">124</dt>
-                <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">New Customers</dd>
-              </dl>
-              <div class="item item-rounded-lg bg-body-light">
-                <i class="far fa-user-circle fs-3 text-primary"></i>
-              </div>
-            </div>
-            <div class="bg-body-light rounded-bottom">
-              <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                <span>View all customers</span>
-                <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-              </a>
-            </div>
-          </div>
-          <!-- END New Customers -->
-        </div>
-        <div class="col-sm-6 col-xxl-3">
-          <!-- Messages -->
-          <div class="block block-rounded d-flex flex-column">
-            <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-              <dl class="mb-0">
-                <dt class="fs-3 fw-bold">45</dt>
-                <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Messages</dd>
-              </dl>
-              <div class="item item-rounded-lg bg-body-light">
-                <i class="far fa-paper-plane fs-3 text-primary"></i>
-              </div>
-            </div>
-            <div class="bg-body-light rounded-bottom">
-              <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                <span>View all messages</span>
-                <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-              </a>
-            </div>
-          </div>
-          <!-- END Messages -->
-        </div>
-        <div class="col-sm-6 col-xxl-3">
-          <!-- Conversion Rate -->
-          <div class="block block-rounded d-flex flex-column">
-            <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-              <dl class="mb-0">
-                <dt class="fs-3 fw-bold">4.5%</dt>
-                <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Conversion Rate</dd>
-              </dl>
-              <div class="item item-rounded-lg bg-body-light">
-                <i class="fa fa-chart-bar fs-3 text-primary"></i>
-              </div>
-            </div>
-            <div class="bg-body-light rounded-bottom">
-              <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                <span>View statistics</span>
-                <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-              </a>
-            </div>
-          </div>
-          <!-- END Conversion Rate-->
-        </div>
-      </div>
-      <!-- END Overview -->
-
       <!-- Statistics -->
       <div class="row">
         <div class="col-xl-8 col-xxl-9 d-flex flex-column">
           <!-- Earnings Summary -->
           <div class="block block-rounded flex-grow-1 d-flex flex-column">
             <div class="block-header block-header-default">
-              <h3 class="block-title">Earnings Summary</h3>
-              <div class="block-options">
-                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
-                  <i class="si si-refresh"></i>
-                </button>
-                <button type="button" class="btn-block-option">
-                  <i class="si si-settings"></i>
-                </button>
-              </div>
+              <h3 class="block-title">How many words you wrote on the Chapter page</h3>
             </div>
             <div class="block-content block-content-full flex-grow-1 d-flex align-items-center">
               <!-- Earnings Chart Container -->
@@ -156,101 +61,33 @@
                   <dl class="mb-0">
                     <dt class="fs-3 fw-bold d-inline-flex align-items-center space-x-2">
                       <i class="fa fa-caret-up fs-base text-success"></i>
-                      <span>2.5%</span>
+                      <span>25</span>
                     </dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">Customer Growth</dd>
+                    <dd class="fs-sm fw-medium text-muted mb-0">Words/Week</dd>
                   </dl>
                 </div>
                 <div class="col-sm-4">
                   <dl class="mb-0">
                     <dt class="fs-3 fw-bold d-inline-flex align-items-center space-x-2">
                       <i class="fa fa-caret-up fs-base text-success"></i>
-                      <span>3.8%</span>
+                      <span>50</span>
                     </dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">Page Views</dd>
+                    <dd class="fs-sm fw-medium text-muted mb-0">Words/Month</dd>
                   </dl>
                 </div>
                 <div class="col-sm-4">
                   <dl class="mb-0">
                     <dt class="fs-3 fw-bold d-inline-flex align-items-center space-x-2">
                       <i class="fa fa-caret-down fs-base text-danger"></i>
-                      <span>1.7%</span>
+                      <span>199</span>
                     </dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">New Products</dd>
+                    <dd class="fs-sm fw-medium text-muted mb-0">Words/Year</dd>
                   </dl>
                 </div>
               </div>
             </div>
           </div>
           <!-- END Earnings Summary -->
-        </div>
-        <div class="col-xl-4 col-xxl-3 d-flex flex-column">
-          <!-- Last 2 Weeks -->
-          <!-- Chart.js Charts is initialized in js/pages/be_pages_dashboard.min.js which was auto compiled from _js/pages/be_pages_dashboard.js -->
-          <!-- For more info and examples you can check out http://www.chartjs.org/docs/ -->
-          <div class="row row-deck flex-grow-1">
-            <div class="col-md-6 col-xl-12">
-              <div class="block block-rounded d-flex flex-column">
-                <div class="block-content flex-grow-1 d-flex justify-content-between">
-                  <dl class="mb-0">
-                    <dt class="fs-3 fw-bold">570</dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">Total Orders</dd>
-                  </dl>
-                  <div>
-                    <div class="d-inline-block px-2 py-1 rounded-3 fs-xs fw-semibold text-danger bg-danger-light">
-                      <i class="fa fa-caret-down me-1"></i>
-                      2.2%
-                    </div>
-                  </div>
-                </div>
-                <div class="block-content p-1 text-center overflow-hidden">
-                  <!-- Total Orders Chart Container -->
-                  <canvas id="js-chartjs-total-orders" style="height: 90px;"></canvas>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-xl-12">
-              <div class="block block-rounded d-flex flex-column">
-                <div class="block-content flex-grow-1 d-flex justify-content-between">
-                  <dl class="mb-0">
-                    <dt class="fs-3 fw-bold">$5,234.21</dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">Total Earnings</dd>
-                  </dl>
-                  <div>
-                    <div class="d-inline-block px-2 py-1 rounded-3 fs-xs fw-semibold text-success bg-success-light">
-                      <i class="fa fa-caret-up me-1"></i>
-                      4.2%
-                    </div>
-                  </div>
-                </div>
-                <div class="block-content p-1 text-center overflow-hidden">
-                  <!-- Total Earnings Chart Container -->
-                  <canvas id="js-chartjs-total-earnings" style="height: 90px;"></canvas>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-12">
-              <div class="block block-rounded d-flex flex-column">
-                <div class="block-content flex-grow-1 d-flex justify-content-between">
-                  <dl class="mb-0">
-                    <dt class="fs-3 fw-bold">264</dt>
-                    <dd class="fs-sm fw-medium text-muted mb-0">New Customers</dd>
-                  </dl>
-                  <div>
-                    <div class="d-inline-block px-2 py-1 rounded-3 fs-xs fw-semibold text-success bg-success-light">
-                      <i class="fa fa-caret-up me-1"></i>
-                      9.3%
-                    </div>
-                  </div>
-                </div>
-                <div class="block-content p-1 text-center overflow-hidden">
-                  <!-- New Customers Chart Container -->
-                  <canvas id="js-chartjs-new-customers" style="height: 90px;"></canvas>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- END Last 2 Weeks -->
         </div>
       </div>
       <!-- END Statistics -->
